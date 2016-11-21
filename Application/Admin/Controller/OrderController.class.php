455d4eebe9e02fba67766ac87c2016e45ec5cae8
Administrator <Administrator@PC-20120726INJR>
2016-11-21 17:29:37 +0800

    更新order

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -6,12 +6,43 @@ use Think\Controller;
 */
 class OrderController extends Controller
 {
-	
+	/**
+	 * 持仓订单列表
+	 */
 	public function Orderlist()
 	{
 		$list = D('order')->get_list();
 		$this->assign('list',$list);
 		$this->display();
+	}
+	/**
+	 * 持仓订单详情
+	 * @return [type] [description]
+	 */
+	public function details(){
+		$id = I('get.uid');
+		$list = D('order')->get_details($id);
+		$this->assign('list',$list);
+		$this->display('orderdetails');
+	}
+	/**
+	 * 成交订单列表
+	 */
+	public function Orderolist()
+	{
+		$list = D('order')->get_olist();
+		$this->assign('list',$list);
+		$this->display();
+	}
+	/**
+	 * 成交订单详情
+	 */
+	public function odetails()
+	{
+		$id = I('get.uid');
+		$list = D('order')->get_odetails($id);
+		$this->assign('list',$list);
+		$this->display('Orderodetails');
 	}
 }
 ?>
