9bbc05d1a7341aa0abbb1fe51ea53d9db28e86a3
zhangbing <285017178@qq.com>
2016-11-21 09:31:25 +0800

    login

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -2,6 +2,11 @@
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    public function login(){
+
+        $this->display();
+    }
+
     public function index(){
         
         $this->display();
