f883a5d1745e2b893407391bf236f061f9b37161
qq552440420 <qq552440420@qq.com>
2016-11-22 17:37:41 +0800

    1

--- Application/Server/Controller/OrderServerController.class.php
+++ Application/Server/Controller/OrderServerController.class.php
@@ -1,3 +1,19 @@
 <?php
+namespace Server\Controller;
+use Think\Controller;
+/**
+* 
+*/
+class OrderServerController extends Controller
+{
+	/**
+	 * 持仓订单列表
+	 */
+	public function Orderlist()
+    {
+        
+       
 
+    }
+}
 ?>
