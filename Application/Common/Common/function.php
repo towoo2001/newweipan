68c2f8f5de349628b354f22b373398747315e46c
tangyijun <251784425@qq.com>
2016-11-22 10:42:49 +0800

    分页方法  function 里面 有调用的实例

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -6,22 +6,38 @@ function pre($arr){
     echo '</pre>';
 }
 /**
- *
- * @param unknown $count  总数
- * @param number $pagesize  每页显示条数
- * @param unknown $row  查询条件
+ * TODO 基础分页的相同代码封装，使前台的代码更少
+ * @param $m 模型，引用传递
+ * @param $where 查询条件
+ * @param int $pagesize 每页查询条数
+ * @return \Think\Page
  */
-function page($count,$pagecount=10,$row = array()){
-    $page = new \Think\Page($count , $pagecount);
-    $page->parameter = $row; //此处的row是数组，为了传递查询条件
-    $page->setConfig('first','首页');
-    $page->setConfig('prev','上一页');
-    $page->setConfig('next','下一页');
-    $page->setConfig('last','尾页');
-    $page->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% 第 '.I('p',1).' 页/共 %TOTAL_PAGE% 页 ( '.$pagecount.' 条/页 共 %TOTAL_ROW% 条)');
-    return $page;
-    
+function getpage(&$m,$where,$pagesize=10){
+    $m1=clone $m;//浅复制一个模型
+    $count = $m->where($where)->count();//连惯操作后会对join等操作进行重置
+    $m=$m1;//为保持在为定的连惯操作，浅复制一个模型
+    $p=new Think\Page($count,$pagesize);
+    $p->lastSuffix=false;
+    $p->setConfig('header','<li class="rows">共<b>%TOTAL_ROW%</b>条记录&nbsp;&nbsp;每页<b>%LIST_ROW%</b>条&nbsp;&nbsp;第<b>%NOW_PAGE%</b>页/共<b>%TOTAL_PAGE%</b>页</li>');
+    $p->setConfig('prev','上一页');
+    $p->setConfig('next','下一页');
+    $p->setConfig('last','末页');
+    $p->setConfig('first','首页');
+    $p->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
+    $p->parameter=I('get.');
+    $m->limit($p->firstRow,$p->listRows);
+    return $p;
 }
+/**
+ * 分页方法调用实例
+ * $m=M('products');
+ * $p=getpage($m,$where,10);
+ * $list=$m->field(true)->where($where)->order('id desc')->select();
+ * $this->list=$list;
+ * $this->page=$p->show();
+ * 
+ */
+
 
 /**
  * 无限极分类
