e1aa5f5d09e40c7378adc435506ca085ad76fe73
qq552440420 <qq552440420@qq.com>
2016-11-22 19:54:19 +0800

    更新server层

--- Application/Server/Controller/OrderServerController.class.php
+++ Application/Server/Controller/OrderServerController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Server\Controller;
 use Think\Controller;
+use Server\Model\OrderServerModel;
 /**
 * 
 */
@@ -11,9 +12,57 @@ class OrderServerController extends Controller
 	 */
 	public function Orderlist()
     {
-        
-       
+       $model = new OrderServerModel();
+       $list = $model->get_list();
+       return $list;
 
     }
+    /**
+	 * 成交订单列表
+	 */
+	public function Orderolist()
+    {
+       $model = new OrderServerModel();
+       $list = $model->get_olist();
+       return $list;
+
+    }
+
+    /**
+	 * 持仓订单详情
+	 */
+	public function Orderdetails()
+    {
+       $id = I('get.oid');
+       $model = new OrderServerModel();
+       $list = $model->get_details($id);
+       return $list;
+
+    }
+     /**
+	 * 成交订单详情
+	 */
+	public function Orderodetails()
+    {
+       $id = I('get.oid');
+       $model = new OrderServerModel();
+       $list = $model->get_odetails($id);
+       return $list;
+
+    }
+ 	/**
+	 * 持仓搜索列表
+	 */
+	public function seach()
+    {
+       
+       $model = new OrderServerModel();
+       $list = $model->seach_list();
+       return $list;
+
+    }
+
+    
+
 }
 ?>
